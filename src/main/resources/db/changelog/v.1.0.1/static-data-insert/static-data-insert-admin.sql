3d26e08ddebd12f031f1f62402fe068840601ed2
Nikolay Kirilyuk <kirabb123@gmail.com>
2024-12-01 17:12:25 +0300

    fixing

--- src/main/resources/db/changelog/v.1.0.1/static-data-insert/static-data-insert-admin.sql
+++ src/main/resources/db/changelog/v.1.0.1/static-data-insert/static-data-insert-admin.sql
@@ -1,8 +1,5 @@
-insert into users (id, first_name, username, password, role
-)
+insert into users (id, first_name, username, password, role)
 values ('53ac777f-fa99-4e47-875d-00782a0b8049',
-        'admin', 'admin@gmail.com',
+        'admin', 'admin',
         '$2a$12$S7cWyHkf6MwtYPBuOsUpvurRD3ORgeLj48wR4N3avFWUattJCqIFe',
-        'ROLE_ADMIN'
---            , now()
-       );
+        'ROLE_ADMIN');
---
+++ src/main/resources/db/changelog/v.1.0.1/static-data-insert/static-data-update-set-work-email.sql
@@ -0,0 +1,3 @@
+UPDATE users
+SET work_email='admin@iase24.com'
+where id = '53ac777f-fa99-4e47-875d-00782a0b8049';
