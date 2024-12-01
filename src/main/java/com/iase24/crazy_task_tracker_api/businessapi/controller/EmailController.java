7a5e4a449c2f12e266a5465e1871f1d207c48133
Nikolay Kirilyuk <kirabb123@gmail.com>
2024-12-01 23:32:49 +0300

    fix

--- src/main/java/com/iase24/crazy_task_tracker_api/businessapi/controller/EmailController.java
+++ src/main/java/com/iase24/crazy_task_tracker_api/businessapi/controller/EmailController.java
@@ -3,7 +3,6 @@ package com.iase24.crazy_task_tracker_api.businessapi.controller;
 import com.iase24.crazy_task_tracker_api.businessapi.service.EmailService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
