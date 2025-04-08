9b2fc0692d6c0299a2d38c1366d4d7e15616a28e
Nikolay Kirilyuk <kirabb123@gmail.com>
2025-04-08 12:23:26 +0300

    enabled scheduler

--- src/main/java/com/iase24/crazy_task_tracker_api/CrazyTaskTrackerApiApplication.java
+++ src/main/java/com/iase24/crazy_task_tracker_api/CrazyTaskTrackerApiApplication.java
@@ -2,8 +2,10 @@ package com.iase24.crazy_task_tracker_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CrazyTaskTrackerApiApplication {
 
     public static void main(String[] args) {
